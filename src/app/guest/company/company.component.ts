0abdd3c80908f94923297fb7772bd2d76d94bf99
vanh2001 <anhviet1709@gmail.com>
2022-07-21 14:22:32 +0700

    update

--- src/app/guest/company/company.component.ts
+++ src/app/guest/company/company.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {CompanyService} from "../../service/company.service";
-import {Company} from "../../model/company";
+import {CompanyService} from '../../service/company.service';
+import {Company} from '../../model/company';
 
 @Component({
   selector: 'app-company',
@@ -14,14 +14,12 @@ export class CompanyComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getAll()
+    this.getAll();
   }
 
   getAll() {
     this.companyService.findAll().subscribe(result => {
-
       this.company = result;
-
     }, error => {
       console.log(error);
     });
--- src/app/service/company.service.ts
+++ src/app/service/company.service.ts
@@ -1,20 +1,22 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {Observable} from 'rxjs';
 import {Company} from '../model/company';
 import {RecruitmentNews} from '../model/recruitment-news';
 
 const API_URL = environment.apiUrl;
+
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
-  private API_COMPANY = environment.apiUrl  +'/companies';
+  private API_COMPANY = environment.apiUrl + '/companies';
   private API_RECRUIMENT_LIST = environment.apiUrl + '/recruitment-news';
-  private API_COMPANY_APPROVED = environment.apiUrl +'/companies'+ '/approved-company';
-  private API_COMPANY_PENDING = environment.apiUrl +'/companies'+ '/pending-company';
-  private API_COMPANY_BLOCK = environment.apiUrl +'/companies'+ '/locked-company';
+  private API_COMPANY_APPROVED = environment.apiUrl + '/companies' + '/approved-company';
+  private API_COMPANY_PENDING = environment.apiUrl + '/companies' + '/pending-company';
+  private API_COMPANY_BLOCK = environment.apiUrl + '/companies' + '/locked-company';
+
   constructor(private httpClient: HttpClient) {
 
   }
@@ -27,24 +29,28 @@ export class CompanyService {
   update(id: any, company: Company): Observable<Company> {
     return this.httpClient.put<Company>(`${API_URL}/companies/${id}`, company);
   }
-findAllApprovedCompany(): Observable<Company[]>{
-  return this.httpClient.get<Company[]>(this.API_COMPANY_APPROVED);
-}
-  findAllPendingCompany(): Observable<Company[]>{
+
+  findAllApprovedCompany(): Observable<Company[]> {
+    return this.httpClient.get<Company[]>(this.API_COMPANY_APPROVED);
+  }
+
+  findAllPendingCompany(): Observable<Company[]> {
     return this.httpClient.get<Company[]>(this.API_COMPANY_PENDING);
   }
-  findAllBlockCompany(): Observable<Company[]>{
+
+  findAllBlockCompany(): Observable<Company[]> {
     return this.httpClient.get<Company[]>(this.API_COMPANY_BLOCK);
   }
 
 
   updateStatus(id: string, company: Company): Observable<Company> {
-    return this.httpClient.put<Company>(`${this.API_COMPANY + '/update-status'}/${id}`, company)
+    return this.httpClient.put<Company>(`${this.API_COMPANY + '/update-status'}/${id}`, company);
   }
 
   findCompanyById(id: string): Observable<Company> {
-    return this.httpClient.get<Company>(`${this.API_COMPANY}/${id}`)
+    return this.httpClient.get<Company>(`${this.API_COMPANY}/${id}`);
   }
+
   findAllRecruiment(): Observable<RecruitmentNews[]> {
     return this.httpClient.get<RecruitmentNews[]>(this.API_RECRUIMENT_LIST);
   }
