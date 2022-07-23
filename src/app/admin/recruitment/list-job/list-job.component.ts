b88b66508fb6c9a8a74c42cb10ca6fa30c7187d0
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-07-23 15:40:45 +0700

    propose tin tuyển dụng

--- src/app/admin/recruitment/list-job/list-job.component.ts
+++ src/app/admin/recruitment/list-job/list-job.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RecruitmentNews} from '../../../model/recruitment-news';
 import {CompanyService} from '../../../service/company.service';
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-list-job',
@@ -8,15 +9,37 @@ import {CompanyService} from '../../../service/company.service';
   styleUrls: ['./list-job.component.css']
 })
 export class ListJobComponent implements OnInit {
-  cruimentnews:RecruitmentNews[]=[];
-  constructor(private companyService: CompanyService) { }
+  recruimentNews: RecruitmentNews[] = [];
+
+  constructor(private companyService: CompanyService) {
+  }
 
   ngOnInit() {
     this.listRecruiment();
   }
-listRecruiment(){
-   this.companyService.findAllRecruiment().subscribe(data=>{
-     this.cruimentnews=data;
-   })
-}
+
+  listRecruiment() {
+    this.companyService.findAllRecruiment().subscribe(data => {
+      this.recruimentNews = data;
+    })
+  }
+
+  proposed(id: string) {
+    this.companyService.proposeCompany(id).subscribe(() => {
+      this.messagePropose();
+      this.listRecruiment();
+    }, error => {
+      alert("Lỗi");
+    })
+  }
+
+  messagePropose() {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Đã khóa doanh nghiệp',
+      showConfirmButton: false,
+      timer: 2000
+    })
+  }
 }
