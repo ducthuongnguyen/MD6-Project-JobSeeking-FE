cc659036cb36cc24c70bc1a9d3d96f3623f2b540
PhamhaiC02222I1 <99933717+PhamhaiC02222I1@users.noreply.github.com>
2022-07-25 09:24:06 +0700

    comit

--- src/app/admin/company/company-list/company-list.component.ts
+++ src/app/admin/company/company-list/company-list.component.ts
@@ -31,6 +31,28 @@ export class CompanyListComponent implements OnInit {
       alert("Lỗi");
     })
   }
+  // private getListRequest(request) {
+  //   this.loading = true;
+  //   this.companyService.findPageUnlockCompany(request).subscribe(data => {
+  //     this.companies = data['content'];
+  //     console.log('data[content]--------', data['content']);
+  //     this.totalElements = data['totalElements'];
+  //     this.loading = false;
+  //   }, error => {
+  //     this.loading = false;
+  //   });
+  // }
+
+  // nextPage(event: PageEvent) {
+  //   console.log('event=====', event);
+  //   const request = {};
+  //   request['page'] = event.pageIndex.toString();
+  //   request['size'] = event.pageSize.toString();
+  //   console.log('request[size]=====', request['size']);
+  //   this.getListRequest(request);
+  //
+  //   // this.getSearchRequest(request,this.name);
+  // }
 
   updateStatus(id: string) {
     this.companyService.updateStatus(id).subscribe(() => {
--- src/app/service/company.service.ts
+++ src/app/service/company.service.ts
@@ -31,9 +31,14 @@ export class CompanyService {
     return this.httpClient.get<Company[]>(this.API_COMPANY);
   }
 
+  // findPageUnlockCompany(request){
+  //   const params = request;
+  //   return  this.httpClient.get(this.API_COMPANY_UNBLOCK,{params});
+  // }
   findUnlockCompany():Observable<Company[]> {
     return this.httpClient.get<Company[]>(this.API_COMPANY_UNBLOCK);
   }
+
 
   update(id: any, company: Company): Observable<Company> {
     return this.httpClient.put<Company>(`${API_URL}/companies/${id}`, company);
