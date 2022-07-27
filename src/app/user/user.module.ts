d26764bd21d2eca9f73cd1c838c5fef2977e1d9a
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-07-27 17:22:28 +0700

    Update user.module.ts

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,41 +1,42 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 // @ts-ignore
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 
-import { UserRoutingModule } from './user-routing.module';
-import { RecruitmentListComponent } from './recruitment/recruitment-list/recruitment-list.component';
+import {UserRoutingModule} from './user-routing.module';
+import {RecruitmentListComponent} from './recruitment/recruitment-list/recruitment-list.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { UserRecruitmentDetailComponent } from './recruitment/user-recruitment-detail/user-recruitment-detail.component';
-import { CompanyListComponent } from './company/company-list/company-list.component';
-import { RecruitmentByCompanyComponent } from './recruitment/recruitment-by-company/recruitment-by-company.component';
-import { UserEditComponent } from './user-edit/user-edit.component';
+import {UserRecruitmentDetailComponent} from './recruitment/user-recruitment-detail/user-recruitment-detail.component';
+import {CompanyListComponent} from './company/company-list/company-list.component';
+import {RecruitmentByCompanyComponent} from './recruitment/recruitment-by-company/recruitment-by-company.component';
+import {UserEditComponent} from './user-edit/user-edit.component';
 import {MatPaginatorModule} from '@angular/material';
-import { FindByUserComponent } from './recruitment/find-by-user/find-by-user.component';
+import {FindByUserComponent} from './recruitment/find-by-user/find-by-user.component';
 
 
 // @ts-ignore
 @NgModule({
   declarations: [
 
-  RecruitmentListComponent,
+    RecruitmentListComponent,
 
-  UserRecruitmentDetailComponent,
+    UserRecruitmentDetailComponent,
 
-  CompanyListComponent,
+    CompanyListComponent,
 
-  RecruitmentByCompanyComponent,
+    RecruitmentByCompanyComponent,
 
-  UserEditComponent,
+    UserEditComponent,
 
-  FindByUserComponent
+    FindByUserComponent
 
   ],
-    imports: [
-        CommonModule,
-        UserRoutingModule,
-        ReactiveFormsModule,
-        FormsModule,
-        MatPaginatorModule,
-    ]
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatPaginatorModule,
+  ]
 })
-export class UserModule { }
+export class UserModule {
+}
