832f581cc803f34bca9b60bb88d940f1624eb6ca
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-07-27 17:13:05 +0700

    update

--- src/app/guest/recruitment/recruitment-unlock/recruitment.component.ts
+++ src/app/guest/recruitment/recruitment-unlock/recruitment.component.ts
@@ -8,75 +8,74 @@ import {error} from "protractor";
 import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
-    selector: 'app-recruitment',
-    templateUrl: './recruitment.component.html',
-    styleUrls: ['./recruitment.component.css']
+  selector: 'app-recruitment',
+  templateUrl: './recruitment.component.html',
+  styleUrls: ['./recruitment.component.css']
 })
 export class RecruitmentComponent implements OnInit {
-    recruitmentNews: RecruitmentNews[] = [];
-    cities: any[] = [];
-    fields: any[] = [];
-    title: any[] = [];
-    checkList;
+  recruitmentNews: RecruitmentNews[] = [];
+  cities: any[] = [];
+  fields: any[] = [];
+  title: any[] = [];
+  checkList;
 
 
-    searchForm: FormGroup = new FormGroup({
-        title: new FormControl(''),
-        cities: new FormControl('')
+  searchForm: FormGroup = new FormGroup({
+    title: new FormControl(''),
+    cities: new FormControl('')
+  });
+
+  constructor(private recruitmentNewService: RecruitmentNewsService,
+              private authenticationService: AuthenticationService,
+              private fieldService: FieldService) {
+  }
+
+  ngOnInit() {
+    this.findAllCity();
+    this.findAllField();
+    this.findUnlockRecruitmentNews();
+  }
+
+  findAllCity() {
+    this.authenticationService.findAllCity().subscribe(result => {
+      this.cities = result;
+    }, error => {
+      alert("Loi !!!");
     });
+  }
 
-    constructor(private recruitmentNewService: RecruitmentNewsService,
-                private authenticationService: AuthenticationService,
-                private fieldService: FieldService) {
-    }
+  findAllField() {
+    this.fieldService.findAll().subscribe((fields: Field[]) => {
+      this.fields = fields;
+    }, error => {
+      alert('Lỗi');
+    });
+  }
 
-    ngOnInit() {
-        this.findAllCity();
-        this.findAllField();
-        this.findUnlockRecruitmentNews();
-    }
+  findUnlockRecruitmentNews() {
+    this.recruitmentNewService.findUnlockRecruitmentNews().subscribe((result: RecruitmentNews[]) => {
+      // @ts-ignore
+      this.recruitmentNews = result;
+      if (this.recruitmentNews.length == 0) {
+        this.checkList = true;
+      }
+    }, error => {
+      alert(error);
+    });
+  }
 
-    findAllCity() {
-        this.authenticationService.findAllCity().subscribe(result => {
-            this.cities = result;
-            if (this.recruitmentNews.length == 0) {
-                this.checkList = true;
-            }
-        }, error => {
-            alert("Loi !!!");
-        });
-    }
-
-    findAllField() {
-        this.fieldService.findAll().subscribe((fields: Field[]) => {
-            this.fields = fields;
-        }, error => {
-            alert('Lỗi');
-        });
-    }
-
-    findUnlockRecruitmentNews() {
-        this.recruitmentNewService.findUnlockRecruitmentNews().subscribe((result: RecruitmentNews[]) => {
-            // @ts-ignore
-            this.recruitmentNews = result;
-        }, error => {
-            alert(error);
-        });
-    }
-
-    findByTitleWorkingPlace() {
-        this.recruitmentNewService.findByTitleWorkingPlace(
-            this.searchForm.value.title,
-            this.searchForm.value.cities).subscribe(result => {
-            // @ts-ignore
-            this.recruitmentNews = result;
-            if (this.recruitmentNews.length == 0) {
-                this.checkList = true;
-            }
-            console.log(this.checkList)
-        }, error => {
-            alert("Loi !!!")
-        });
-        this.checkList = false;
-    }
+  findByTitleWorkingPlace() {
+    this.recruitmentNewService.findByTitleWorkingPlace(
+      this.searchForm.value.title,
+      this.searchForm.value.cities).subscribe(result => {
+      // @ts-ignore
+      this.recruitmentNews = result;
+      if (this.recruitmentNews.length == 0) {
+        this.checkList = true;
+      }
+    }, error => {
+      alert("Loi !!!")
+    });
+    this.checkList = false;
+  }
 }
